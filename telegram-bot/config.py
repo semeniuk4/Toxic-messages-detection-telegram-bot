1f5af87dbb6f02918e6abe660e09d513f68eba50
Ivan Semeniuk <ivan.semeniuk2004@gmail.com>
2023-09-02 10:52:12 +0200

    Updated

--- telegram-bot/config.py
+++
@@ -1,3 +0,0 @@
-TELEGRAM_BOT_TOKEN = '6605112738:AAE76l-ERhwXhwi6DibgCfYMiSGzmuuwIPI'
-NGROK_TUNNEL_URL = 'https://183b-147-232-176-28.ngrok-free.app'
-DOCKER_API_URL = 'http://0.0.0.0:32768/predict'
